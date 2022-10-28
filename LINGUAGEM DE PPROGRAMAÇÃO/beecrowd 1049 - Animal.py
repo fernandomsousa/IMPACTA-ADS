a33964fd1082ac0cc4aed6c5657c20dd58b1874d
FernandoM <fernandom@pop-os.localdomain>
2022-10-28 15:22:20 -0300

    Primeiro Commit

---
+++ LINGUAGEM DE PPROGRAMAÇÃO/beecrowd 1049 - Animal.py
@@ -0,0 +1,27 @@
+X = (input())
+Y = (input())
+Z = (input())
+
+if X == "vertebrado" and Y == "ave" and Z == "carnivoro":
+    print("aguia")
+    
+if X == "vertebrado" and Y == "ave" and Z == "onivoro":
+    print("pomba")
+
+if X == "vertebrado" and Y == "mamifero" and Z == "onivoro":
+    print("homem")
+    
+if X == "vertebrado" and Y == "mamifero" and Z == "herbivoro":
+    print("vaca")
+
+if X == "invertebrado" and Y == "inseto" and Z == "hematofago":
+    print("pulga")
+    
+if X == "invertebrado" and Y == "inseto" and Z == "herbivoro":
+    print("lagarta")
+    
+if X == "invertebrado" and Y == "anelideo" and Z == "hematofago":
+    print("sanguessuga")
+    
+if X == "invertebrado" and Y == "anelideo" and Z == "onivoro":
+    print("minhoca")
